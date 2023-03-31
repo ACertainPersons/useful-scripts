d69786974e4613529e6628402fc4a8606f33c32d
ACertainPersons <127463096+ACertainPersons@users.noreply.github.com>
2023-03-30 21:06:55 -0500

    message

---
+++ github.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+str="$(git status 2>&1)"
+substr="fatal"
+if [[ $str == *"$substr"* ]];
+then
+    echo "fatal: not a git repository (or any of the parent directories): .git"
+else
+    str="$(git status 2>&1)"
+    substr="nothing to commit"
+    if [[ $str == *"$substr"* ]];
+    then
+        echo "It is up to date! Nothing to commit. :-)"
+    else
+        git add --all
+        git status
+        echo "What is your commit message?"
+        read message
+        git commit -m "message"
+        echo "Which branch do you want to push your commit to?"
+        read branch
+        git push origin branch
+    fi
+fi
+echo ""
