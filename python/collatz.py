2eea14ab1bdf87734de6c92fc8def31c404b575b
ACertainPersons <127463096+ACertainPersons@users.noreply.github.com>
2023-09-20 17:00:33 -0500

    modified to include decimals and 0

--- python/collatz.py
+++ python/collatz.py
@@ -1,19 +1,22 @@
 t=0
 try :
     n = int(input("Insert number here: "))
-    if n>0 :
-        while n!=1:
+    if n!=0 :
+        while True:
             print (n)
             if n%2==1 :
                 n= 3*n+1
             else :
                 n= n/2
             t+=1
-        print ("1.0")
-        print ()
-        print ("steps taken:")
-        print (t)
-    else: 
-        print("Negative numbers (and 0) not supported yet")
+            if n==1 or n==-1 or n==-10 or n==-34:
+                print(n)
+                break
+    elif n==0:
+        print(0.0)
+        t=1
+    print ()
+    print ("steps taken:")
+    print (t)
 except ValueError:
     print("Only integers are supported :-(")
