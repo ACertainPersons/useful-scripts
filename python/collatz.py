23aeed70918070ff65e8c3d9b601e23f26041259
ACertainPersons <127463096+ACertainPersons@users.noreply.github.com>
2023-10-03 11:21:19 -0500

    Added documentation

--- python/collatz.py
+++ python/collatz.py
@@ -3,13 +3,13 @@ try :
     n = int(input("Insert number here: "))
     if n!=0 :
         while True:
-            print (n)
+            print (n) #This is the actual 3x+1 part
             if n%2==1 :
                 n= 3*n+1
             else :
                 n= n/2
             t+=1
-            if n==1 or n==-1 or n==-10 or n==-34:
+            if n==1 or n==-1 or n==-10 or n==-34: #To prevent infinite loops by breaking it if it reaches the end of a loop
                 print(n)
                 break
     elif n==0:
--- python/pi.py
+++ python/pi.py
@@ -1,14 +1,13 @@
 import math
-import decimal
 
-def sigma(times):
+def sigma(times): #Named after sigma notation
     a = (0,)
     n=0
     for n in range(times):
-        n+=1
+        n+=1 #To calculate the number of times
         a = a + (n,)
-        m = tuple([((((-1)**q)*math.factorial(6*q)*(545140134*q + 13591409))/(math.factorial(3*q)*((math.factorial(q)**3)*(640320**(3*q+(3/2)))))) for q in a])
-    x = sum(m)
+        m = tuple([((((-1)**q)*math.factorial(6*q)*(545140134*q + 13591409))/(math.factorial(3*q)*((math.factorial(q)**3)*(640320**(3*q+(3/2)))))) for q in a]) # Tuple used to store a set of number, algorithm used is the Chudnovsky Algorithm (https://en.wikipedia.org/wiki/Chudnovsky_algorithm)
+    x = sum(m) #The set fo numbers is added toghether
     return x
         
 print((12*sigma(17))**(-1))
