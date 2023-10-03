9b7e48c246132140fb7f4e0b32f858a857b69eb9
ACertainPersons <127463096+ACertainPersons@users.noreply.github.com>
2023-10-02 20:29:48 -0500

    bug fix for bigtime.py

--- python/BigTime.py
+++ python/BigTime.py
@@ -3,9 +3,6 @@ import time
 import os
 from datetime import datetime
 
-hrs = datetime.now().hour
-mins = datetime.now().minute
-sec = datetime.now().second
 print("Paste this in your browser for the font list: \nhttps://www.ascii-art.site/FontList.html\n")
 font = input("Font for the clock (default font will be used if not specified): ")
 
@@ -16,10 +13,19 @@ def output(x):
     os.system('clear')
     tprint(str(x), font=font)
     time.sleep(0.09)
-
+        
 
 while True:
     hrs = datetime.now().hour
     mins = datetime.now().minute
     sec = datetime.now().second
-    output(str(hrs) + ":" + str(mins) + ":" + str(sec))
+    hrs = str(hrs)
+    if int(hrs) < 10:
+        hrs = "0" + hrs
+    mins = str(mins)
+    if int(mins) < 10:
+        mins = "0" + mins
+    sec = str(sec)
+    if int(sec) < 10:
+        sec = "0" + sec
+    output(hrs + ":" + mins + ":" + sec)
